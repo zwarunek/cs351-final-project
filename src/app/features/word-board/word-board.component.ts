e12696f81b73dca6abc6a12704580e483bac5147
ZWARUNEK <zacharywarunek8@gmail.com>
2022-02-18 13:45:13 -0500

    fixed keyboard winning and losing messages

--- src/app/features/word-board/word-board.component.ts
+++ src/app/features/word-board/word-board.component.ts
@@ -66,20 +66,32 @@ export class WordBoardComponent implements OnInit {
       "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"
     ];
     this.allowedChars = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "enter", "backspace"];
+    this.gameState = 'playing';
   }
   handleKeyPress(key: string){
     this.key = key.toLowerCase();
     if(this.allowedChars.includes(this.key)) {
       let rowElement = document.getElementById('board-row-'+this.currentGuess);
 
-      if (this.key === 'enter') {
+      if (this.key === 'enter' && this.gameState === 'playing') {
         let guess = this.guesses[this.currentGuess].join("");
-        console.log(guess)
         if(guess.length == this.letters && (this.wordlistGuesses.includes(guess) || this.wordlistAnswers.includes(guess))) {
-          this.checkWord(guess, this.word);
           this.currentGuess++;
           this.currentGuessChars = 0;
           this.guessedWords.push(guess);
+          this.checkWord(guess, this.word);
+          if(guess === this.word){
+            this.gameState = 'won';
+            this.messageService.clear();
+            this.messageService.add({severity:'success', sticky: false, summary: 'You Won!',
+              detail:''});
+          }
+          else if(this.currentGuess === this.numberOfGuesses){
+            this.gameState = 'lost'
+            this.messageService.clear();
+            this.messageService.add({severity:'warn', sticky: true, summary: 'You Lost!',
+              detail: 'The word was <b>'+this.word+'</b>'});
+          }
         }
         else{
           if(rowElement) {
@@ -95,8 +107,6 @@ export class WordBoardComponent implements OnInit {
       }
       else if (this.key === 'backspace') {
         if(this.currentGuessChars > 0){
-          // if(rowElement)
-          //   rowElement.classList.remove('invalid')
           this.currentGuessChars --;
           this.guesses[this.currentGuess][this.currentGuessChars]= "";
         }
@@ -118,7 +128,6 @@ export class WordBoardComponent implements OnInit {
     let i = 0;
     const myLoop = () => {
       setTimeout(() => {
-        console.log(i)
         if(guess.charAt(i) === word.charAt(i)){
           word = word.substring(0, i) + '*' + word.substring(i + 1)
           this.guessResults[this.currentGuess-1][i] = 'correct'
@@ -142,36 +151,11 @@ export class WordBoardComponent implements OnInit {
         }
       }, 200)
     }
-
     myLoop();
-    // for(let i = 0; i < guess.length; i++){
-    //   console.log(guess.charAt(i), word.charAt(i), guess.charAt(i) === word.charAt(i))
-    //   if(guess.charAt(i) === word.charAt(i)){
-    //     word = word.substring(0, i) + '*' + word.substring(i + 1)
-    //     this.guessResults[this.currentGuess][i] = 'correct'
-    //     this.keyboardResults[this.keyboardKeys.indexOf(guess.charAt(i))] = 'correct'
-    //   }
-    // }
-    // for(let i = 0; i < guess.length; i++){
-    //   if(word.includes(guess.charAt(i)) && word.charAt(i) !== '*'){
-    //     word = word.substring(0, word.indexOf(guess.charAt(i))) + '-' + word.substring(word.indexOf(guess.charAt(i)) + 1)
-    //     this.guessResults[this.currentGuess][i] = 'present'
-    //     if(this.keyboardResults[this.keyboardKeys.indexOf(guess.charAt(i))] !== 'correct')
-    //       this.keyboardResults[this.keyboardKeys.indexOf(guess.charAt(i))] = 'present'
-    //   }
-    //   else if(word.charAt(i) !== '*'){
-    //     this.guessResults[this.currentGuess][i] = 'unused'
-    //     if(this.keyboardResults[this.keyboardKeys.indexOf(guess.charAt(i))] !== 'correct'
-    //       && this.keyboardResults[this.keyboardKeys.indexOf(guess.charAt(i))] !== 'present')
-    //       this.keyboardResults[this.keyboardKeys.indexOf(guess.charAt(i))] = 'unused'
-    //   }
-    // }
-    console.log(word)
   }
 
   getDefinition(guess: any) {
     this.http.get('https://api.dictionaryapi.dev/api/v2/entries/en/' + guess).subscribe((data: any) => {
-      console.log(data[0].meanings);
       this.messageService.clear();
       this.messageService.add({severity:'custom', sticky: true, summary: guess.charAt(0).toUpperCase() + guess.slice(1),
         detail: (data[0].meanings[0].partOfSpeech.charAt(0).toUpperCase() + data[0].meanings[0].partOfSpeech.slice(1)).bold()
